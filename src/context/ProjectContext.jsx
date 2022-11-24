3f165d53826a369db754c6bacee313a31bbfd7f6
imMatheus <matheusmendesbarata03@gmail.com>
2022-11-24 12:25:22 +0100

    can now change project

--- src/context/ProjectContext.jsx
+++ src/context/ProjectContext.jsx
@@ -13,9 +13,42 @@ const ProjectProvider = ({ children }) => {
     const [currentIndex, setCurrentIndex] = useState(0)
     const currentProject = projects[currentIndex]
 
+    console.log(currentProject)
+
+    const changeBoard = (index) => {
+        setCurrentIndex(index)
+    }
+
+    const addNewProject = (title) => {
+        if (!title) return
+
+        const data = {
+            title: title,
+            id: projects.length + 1,
+            board: [
+                {
+                    name: 'Todo',
+                    tickets: [],
+                },
+                {
+                    name: 'Doing',
+                    tickets: [],
+                },
+                {
+                    name: 'Done',
+                    tickets: [],
+                },
+            ],
+        }
+        setProjects((prev) => [...prev, data])
+        setCurrentIndex(projects.length)
+    }
+
     const value = {
         currentProject,
         projects,
+        changeBoard,
+        addNewProject,
     }
 
     return (
