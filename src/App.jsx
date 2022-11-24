0a55bfcaaa6813c25942deb0115a53283135c9aa
imMatheus <matheusmendesbarata03@gmail.com>
2022-11-24 12:42:13 +0100

    added navbar and made it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar'
 import Board from './components/Board/Board'
 import SideNav from './components/SideNav/SideNav'
 
 function App() {
     return (
         <div className="App">
+            <Navbar />
             <main>
                 <SideNav />
                 <Board />
